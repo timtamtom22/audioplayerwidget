9b91615c689018248f2bc930e131b2e57cb97410
timtamtom22 <thomas.alchin123@gmail.com>
2026-05-17 18:11:19 +0100

    BIG SWITCHERAO

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -1,35 +1,58 @@
 export default [
   {
     id: 1,
-    title: "Electronic Ambiance",
-    audio_url: "/audio/relaxed.wav",
-    thumbnail_url: "/images/np/asjb.jpg",
-    description: "Relaxed and calm background music, written for 'New Protocl'. Itch link is here: https://xujun.itch.io/new-protocol",
-    images: ["/images/np/f_OTdT.png", "/images/np/qoEV2+.png", "/images/np/962zhB.jpg" ]
+    title: "Ethereal Voices",
+    audio_url: "/audio/voices.wav",
+    thumbnail_url: "/images/roots/roots1",
+    description: "Chaotic, Raw, and Pagan inspired, this is a cue from Beyond These Roots, a short film from the Queen Mary University London",
+    images: ["/images/roots/roots1", "/images/roots/roots2", "/images/roots/roots3"]
   },
   {
-    id: 2,
-    title: "Finale",
-    audio_url: "/audio/finale.wav",
-    thumbnail_url: "/images/np/asjb.jpg",
-    description: "Impactful and large electronic's with orchestra, written for 'New Protocol'. Itch link is here: https://xujun.itch.io/new-protocol",
-    images: ["/images/np/f_OTdT.png", "/images/np/qoEV2+.png", "/images/np/962zhB.jpg" ]
+ id: 2,
+    title: "Pecuiliar Fantasy",
+    audio_url: "/audio/orchestral.wav",
+    thumbnail_url: "/images/david/david1.jpg",
+    description: "Mysterious and sweeping, this orchestral cue was inspired and given feedback to by David Buckley, also being recorded by the Guildhall Session Orchestra",
+    images: ["/images/david/david1.jpg", "/images/david/david2.jpg"]
   },
   {
     id: 3,
-    title: "Synthwave Drift",
-    audio_url: "/audio/aim.wav",
-    thumbnail_url: "/images/aim/Hn65To.png",
-    description: "Intense synthwave which collapses into destruction, written for A.I.M. Itch link is here: https://llmxm.itch.io/aim",
-    images: ["/images/aim/lHGkTw.png", "/images/aim/LuF2xY.png", "/images/aim/NWqnsW.png"]
+    title: "Spanish Orchestral Suite",
+    audio_url: "/audio/spanish.wav",
+    thumbnail_url: "/images/spanish/thumbnail.png",
+    description: "Energetic, Honest, and Authentic, this is a cue from  'Into Fire', an animation from MICA, inspired by Pasodobles Festeros",
+    images: ["/images/spanish/spanish1.png", "/images/spanish/spanish2.png", "/images/spanish/spanish3.png"]
   },
   {
     id: 4,
-    title: "Chiptune",
-    audio_url: "/audio/chiptune.mp3",
-    thumbnail_url: "/images/chiptune/lsdj.png",
-    description: "Happy chiptune piece, wrote for personal enjoyment (:",
-    images: ["/images/chiptune/chiptune1.png", "/images/chiptune/chiptune2.png"]
+    title: "Screaming Voices",
+    audio_url: "/audio/horror.m4a",
+    thumbnail_url: "/images/horror/thumbnail.jpg",
+    description: "Horrifying orchestral and organic sounds, based of the images shown below",
+    images: ["/images/horror/horror1.png", "/images/horror/horror2.png"]
+  },
+   {
+ id: 5,
+    title: "Revival of Rave",
+    audio_url: "/audio/rave.wav",
+    thumbnail_url: "/images/rave/rave1",
+    description: "Hard-style and intense, this is a cue from the short film 'Revival of rave' collaboration with LFA. Composed with Manish Sanga for this piece.",
+    images: ["/images/rave/rave2", "/images/rave/rave3"]
+  },
+  {
+    id: 6,
+    title: "Starknight",
+    audio_url: "/audio/starknight.wav",
+    thumbnail_url: "/images/starknight/starknight1.png",
+    description: "Vintage analouge synthesis, a cue from 'Starknight', a short animation collaboration with CSM UAL.",
+    images: ["/images/starknight/starknight21.png", "/images/starknight/starknight2.png", "/images/starknight/starknight3.png"]
+  },
+  {
+    id: 7,
+    title: "Electronic Underscore",
+    audio_url: "/audio/sam.wav",
+    thumbnail_url: "",
+    description: "Electronic and fun, a piece written for 'Sam and the Entites', a short animation collaboration with CSM UAL.",
+    images: []
   }
-
 ];
