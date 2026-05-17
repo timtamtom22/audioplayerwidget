9ce033f2ca2481bd4dea0f36304363d854b06d5d
timtamtom22 <thomas.alchin123@gmail.com>
2026-05-17 16:54:41 +0100

    Fixing references no more /public

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -2,34 +2,34 @@ export default [
   {
     id: 1,
     title: "Electronic Ambiance",
-    audio_url: "/public/audio/relaxed.wav",
-    thumbnail_url: "/public/images/np/asjb.jpg",
+    audio_url: "/audio/relaxed.wav",
+    thumbnail_url: "/images/np/asjb.jpg",
     description: "Relaxed and calm background music, written for 'New Protocl'",
-    images: ["/public/images/np/f_OTdT.png", "/public/images/np/qoEV2+.png", "/public/images/np/962zhB.jpg" ]
+    images: ["/images/np/f_OTdT.png", "/images/np/qoEV2+.png", "/images/np/962zhB.jpg" ]
   },
   {
     id: 2,
     title: "Finale",
-    audio_url: "/public/audio/finale.wav",
-    thumbnail_url: "/public/images/np/asjb.jpg",
+    audio_url: "/audio/finale.wav",
+    thumbnail_url: "/images/np/asjb.jpg",
     description: "Impactful and large electronic's with orchestra, written for 'New Protocol'",
-    images: ["/public/images/np/f_OTdT.png", "/public/images/np/qoEV2+.png", "/public/images/np/962zhB.jpg" ]
+    images: ["/images/np/f_OTdT.png", "/images/np/qoEV2+.png", "/images/np/962zhB.jpg" ]
   },
   {
     id: 3,
     title: "Synthwave Drift",
-    audio_url: "/public/audio/aim.wav",
-    thumbnail_url: "/public/images/np/asjb.jpg",
+    audio_url: "/audio/aim.wav",
+    thumbnail_url: "/images/np/asjb.jpg",
     description: "Intense synthwave which collapses into destruction, written for A.I.M",
-    images: ["public/images/aim/lHGkTw.png", "public/images/aim/LuF2xY.png", "public/images/aim/NWqnsW.png" ]
+    images: ["/images/aim/lHGkTw.png", "/images/aim/LuF2xY.png", "/images/aim/NWqnsW.png"]
   },
   {
     id: 4,
     title: "Chiptune",
-    audio_url: "/public/audio/chiptune.mp3",
-    thumbnail_url: "/public/images/lsdj.png",
+    audio_url: "/audio/chiptune.mp3",
+    thumbnail_url: "/images/chiptune/lsdj.png",
     description: "Happy chiptune piece, wrote for personal enjoyment (:",
-    images: []
+    images: ["/images/chiptune/chiptune1.png", "/images/aim/chiptune2.png"]
   }
 
 ];
